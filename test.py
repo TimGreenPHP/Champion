9db4a7342f39491009e290626cf7104a5c91a73a
TimGreenPHP <timgreen123@163.com>
2019-04-09 14:54:54 +0800

    4.9

--- test.py
+++ test.py
@@ -1,1 +1,34 @@
-print(2**10)
+"""name = input('What\'s Ur name?\n')
+print("Will U marry me,",name,'?')
+print("1024 * 768 =",1024*768)
+# print absolute value of an integer:
+a = 2e10
+if a >= 0:
+    print(a)
+else:
+    print(-a)
+print('''line1
+line2
+line3''')
+print(3>2)
+a = 123
+a = "abc"
+print(a)
+print(int('A',16))
+print(11//3)
+a = ord('A') + ord('中')
+print(chr(a))
+a = b'ABC'
+print('中文'.encode('utf-8'))
+print('%2d-%02d' % (3, 1))
+print('%.2f' % 3.1415926)"""
+classmates = ['Buck','Rocket','Warrior']
+print(len(classmates))
+classmates.append('Laker')
+print(classmates)
+L = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Lisa']
+]
+print(L[0][0],L[1][1],L[2][2])
